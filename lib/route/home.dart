a2172c167e5880f2d6cd41a283c4d11793c4a76d
ooooo333i <antmdrl0531@gmail.com>
2025-05-09 17:57:10 +0900

    add auth

--- lib/route/home.dart
+++ lib/route/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -17,7 +18,19 @@ class _HomeState extends State<Home> {
           style: TextStyle(fontSize: 30, fontWeight: FontWeight.w400),
         ),
         leading: IconButton(onPressed: () {}, icon: Icon(Icons.account_circle)),
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add_box))],
+        actions: [
+          IconButton(
+            onPressed: () {
+              final user = FirebaseAuth.instance.currentUser;
+              if (user == null) {
+                Navigator.pushNamed(context, '/sign-in');
+              } else {
+                Navigator.pushNamed(context, '/profile');
+              }
+            },
+            icon: Icon(Icons.perm_identity),
+          ),
+        ],
       ),
     );
   }
